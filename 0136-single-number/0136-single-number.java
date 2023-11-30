1fd6e78820be9536d80a38f5261daf0abaf5351b
Faizan Mohammed <106433504+FaizanMohd5@users.noreply.github.com>
2023-11-30 23:44:31 +0530

    Update 0136-single-number.java

--- 0136-single-number/0136-single-number.java
+++ 0136-single-number/0136-single-number.java
@@ -1,7 +1,7 @@
 class Solution {
     public int singleNumber(int[] nums) {
         Map<Integer, Integer> map = new HashMap<>();
-        for(int i=0; i<nums.length; i++){
+        for (int num : nums){
             map.put(nums[i], map.getOrDefault(nums[i], 0) + 1);
         }
         for(Map.Entry<Integer, Integer> set: map.entrySet()){
