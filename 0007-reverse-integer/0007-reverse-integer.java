707a3391e8f14d3d476f59e3865245753f4337a1
Faizan Mohammed <106433504+FaizanMohd5@users.noreply.github.com>
2023-12-07 23:44:57 +0530

    Update 0007-reverse-integer.java

--- 0007-reverse-integer/0007-reverse-integer.java
+++ 0007-reverse-integer/0007-reverse-integer.java
@@ -3,9 +3,9 @@ class Solution {
         int reverse = 0;
         int remainder;
         int temp = x;
-        while(temp != 0){
+        while(temp != 0)
+        {
             remainder = temp % 10;
-            // To match constraints
             if(reverse > Integer.MAX_VALUE/10 || reverse < Integer.MIN_VALUE/10)
                 return 0;
             reverse = reverse*10 + remainder;
