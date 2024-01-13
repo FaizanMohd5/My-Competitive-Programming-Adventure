7587793c277e429e96d39660e47ece9acfe59dd3
Faizan Mohammed <106433504+FaizanMohd5@users.noreply.github.com>
2024-01-13 05:52:38 +0530

    Time: 0 ms (100.00%), Space: 46.2 MB (24.08%) - LeetHub

--- 0042-trapping-rain-water/0042-trapping-rain-water.java
+++ 0042-trapping-rain-water/0042-trapping-rain-water.java
@@ -1,38 +1,27 @@
 class Solution {
     public int trap(int[] height) {
         int trappedWater = 0;
+        int left = 0;
+        int right = height.length - 1;
+        int leftMax = height[left];
+        int rightMax = height[right];
         
-        // Auxilary arrays to store the left and right maximum boundaries for each bar.
-        
-        int[] leftMaxBoundary = new int[height.length];
-        int[] rightMaxBoundary = new int[height.length];
-
-        // calculating maximum left boundaries for all bars (start to end index)
-        leftMaxBoundary[0] = height[0]; 
-        for(int i = 1; i < height.length; i++)  {
-            leftMaxBoundary[i] = Math.max(height[i], leftMaxBoundary[i-1]);
+        while(left < right){
+            if(leftMax < rightMax)
+            {
+                left++;
+                if(leftMax < height[left])
+                    leftMax = height[left];
+                trappedWater += leftMax - height[left];
+            }
+            else{
+                right--;
+                if(rightMax < height[right])
+                    rightMax = height[right];
+                trappedWater += rightMax - height[right];
+            }
+            
         }
-        
-        // calculating maximum right boundaries for all bars from right to left (last to first index)
-        rightMaxBoundary[height.length - 1] = height[height.length - 1];     
-        for(int i = height.length - 2; i >= 0; i--) {
-            rightMaxBoundary[i] = Math.max(height[i], rightMaxBoundary[i+1]);
-        }
-            
-        // Calculating trapped water with formula: ( min(leftMaxBoundary, rightMaxBoundary) - currentBarHeight ) * width (1 for all, so we won't multiply 1 in teration)
-            
-        for(int i = 0; i < height.length; i++) {    
-            int waterLevelFromGround = Math.min(leftMaxBoundary[i], rightMaxBoundary[i]);
-            int barHeight = height[i];
-            int surfaceAreaOfWater = waterLevelFromGround - barHeight;
-            
-            //Applying Kadane's algorithm to avoid negative values.
-            if(surfaceAreaOfWater < 0)
-                surfaceAreaOfWater = 0;  
-            
-            trappedWater += surfaceAreaOfWater;
-        }
-        
         return trappedWater;
     }
 }
