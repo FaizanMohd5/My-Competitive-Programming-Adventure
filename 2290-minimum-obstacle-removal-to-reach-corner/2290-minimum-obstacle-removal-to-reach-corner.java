f5a2f1e7b78c0046579b8bbc101755b45a0434d4
Faizan Mohammed <106433504+FaizanMohd5@users.noreply.github.com>
2024-11-29 00:56:39 +0530

    Time: 168 ms (16.25%), Space: 71.8 MB (60.73%) - LeetHub

---
+++ 2290-minimum-obstacle-removal-to-reach-corner/2290-minimum-obstacle-removal-to-reach-corner.java
@@ -0,0 +1,40 @@
+class Solution {
+    public int minimumObstacles(int[][] grid) {
+        int m = grid.length;
+        int n = grid[0].length;
+
+        int[][] result = new int[m][n];
+        for (int[] row : result) {
+            Arrays.fill(row, Integer.MAX_VALUE);
+        }
+        result[0][0] = 0;
+
+        PriorityQueue<int[]> pq = new PriorityQueue<>(Comparator.comparingInt(a -> a[0]));
+        pq.offer(new int[]{0, 0, 0});  
+
+        int[][] directions = {{0, 1}, {0, -1}, {1, 0}, {-1, 0}};
+
+        while (!pq.isEmpty()) {
+            int[] current = pq.poll();
+            int obstacleCount = current[0];
+            int i = current[1];
+            int j = current[2];
+
+            for (int[] dir : directions) {
+                int x = i + dir[0];
+                int y = j + dir[1];
+
+                if (x >= 0 && x < m && y >= 0 && y < n) {
+                    int wt = (grid[x][y] == 1) ? 1 : 0;
+
+                    if (obstacleCount + wt < result[x][y]) {
+                        result[x][y] = obstacleCount + wt;
+                        pq.offer(new int[]{obstacleCount + wt, x, y});
+                    }
+                }
+            }
+        }
+
+        return result[m - 1][n - 1];
+    }
+}
