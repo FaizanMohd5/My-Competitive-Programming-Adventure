dd29a69490b1b74e361420df9bccaf1b042d6685
Faizan Mohammed <106433504+FaizanMohd5@users.noreply.github.com>
2024-12-24 20:57:30 +0530

    Time: 0 ms (100.00%), Space: 48.2 MB (13.64%) - LeetHub

---
+++ 0275-h-index-ii/0275-h-index-ii.java
@@ -0,0 +1,27 @@
+class Solution {
+    public int hIndex(int[] citations) {
+        
+        int n = citations.length;
+        
+        int low = 0;
+        int high = n - 1;
+        
+        int answer = 0;
+        
+        while(low <= high){
+            
+            int mid = low + (high - low) / 2;
+            
+            if(citations[mid] >= n - mid){
+                answer = n - mid;
+                high = mid - 1;
+            }
+            else{
+                low = mid + 1;
+            }
+            
+        }
+        
+        return answer;
+    }
+}
