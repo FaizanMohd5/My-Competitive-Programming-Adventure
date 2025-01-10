f7d94797d43b59186624f86ee86d8a105b708d23
Faizan Mohammed <106433504+FaizanMohd5@users.noreply.github.com>
2025-01-11 04:36:06 +0530

    Time: 12 ms, Memory: 55.2 MB - LeetHub

---
+++ 3409-303-916-word-subsets/3409-303-916-word-subsets.java
@@ -0,0 +1,38 @@
+class Solution {
+    public List<String> wordSubsets(String[] words1, String[] words2) {
+        
+        List<String> result = new ArrayList<>();
+        
+        int[] freq2 = new int[26]; 
+        for (String word : words2) {
+            int[] temp = new int[26]; 
+            for (char ch : word.toCharArray()) {
+                temp[ch - 'a']++;
+                freq2[ch - 'a'] = Math.max(freq2[ch - 'a'], temp[ch - 'a']);
+            }
+        }
+        
+        for (String word : words1) {
+            int[] temp = new int[26]; 
+            for (char ch : word.toCharArray()) {
+                temp[ch - 'a']++;
+            }
+            
+            // Check if the word satisfies the universal condition
+            if (isSubset(freq2, temp)) {
+                result.add(word);
+            }
+        }
+        
+        return result;
+        
+    }
+    private boolean isSubset(int[] freq2, int[] temp) {
+        for (int i = 0; i < 26; i++) {
+            if (temp[i] < freq2[i]) {
+                return false; // Not a subset
+            }
+        }
+        return true;
+    }
+}
