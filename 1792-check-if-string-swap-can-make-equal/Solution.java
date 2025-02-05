bb76481e479832b31018599b290efe66b458692f
Faizan Mohammed <106433504+FaizanMohd5@users.noreply.github.com>
2025-02-06 04:47:03 +0530

    Create Solution.java

---
+++ 1792-check-if-string-swap-can-make-equal/Solution.java
@@ -0,0 +1,28 @@
+class Solution {
+    public boolean areAlmostEqual(String s1, String s2) {
+        
+        int i = -1;
+        int j = -1;
+
+        int count = 0;
+
+        for(int k = 0; k < s1.length(); k++){
+            if(s1.charAt(k) != s2.charAt(k)){
+                count++;
+                if(i == -1){
+                    i = k;
+                }
+                else if(j == -1){
+                    j = k;
+                }
+            }
+        }
+
+        if(count == 0) return true;
+        else if(count == 2 && s1.charAt(i) == s2.charAt(j) && s1.charAt(j) == s2.charAt(i)){
+            return true;
+        }
+
+        return false;
+
+    }
